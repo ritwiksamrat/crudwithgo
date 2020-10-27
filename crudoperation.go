73a1b2499bfb562e879f7cda6e16922b621d6f96
ritwiksamrat <64915010+ritwiksamrat@users.noreply.github.com>
2020-10-27 18:00:37 +0530

    Add files via upload

---
+++ crudoperation.go
@@ -0,0 +1,84 @@
+package main
+ 
+import (
+    "database/sql"
+    "fmt"
+    _ "github.com/go-sql-driver/mysql"
+)
+ 
+type Post struct {
+    Id   int
+    Name string
+    Text string
+}
+ 
+func main() {
+    db, e := sql.Open("mysql", "rootuser:pass1234@/posts")
+    ErrorCheck(e)
+ 
+    
+    defer db.Close()
+ 
+    PingDB(db)
+ 
+   
+    stmt, e := db.Prepare("insert into posts(id, Name, Text) values (?, ?, ?)")
+    ErrorCheck(e)
+ 
+    
+    res, e := stmt.Exec("5", "Post five", "Contents of post 5")
+    ErrorCheck(e)
+ 
+    id, e := res.LastInsertId()
+    ErrorCheck(e)
+ 
+    fmt.Println("Insert id", id)
+ 
+    
+    stmt, e := db.Prepare("update posts set Text=? where id=?")
+    ErrorCheck(e)
+ 
+        res, e := stmt.Exec("This is post five", "5")
+    ErrorCheck(e)
+ 
+    a, e := res.RowsAffected()
+    ErrorCheck(e)
+ 
+    fmt.Println(a)
+ 
+    
+    rows, e := db.Query("select * from posts")
+    ErrorCheck(e)
+ 
+    var post = Post{}
+ 
+    for rows.Next() {
+        e = rows.Scan(&post.Id, &amp;post.Name, &post.Text)
+        ErrorCheck(e)
+        fmt.Println(post)
+    }
+ 
+    
+    stmt, e := db.Prepare("delete from posts where id=?")
+    ErrorCheck(e)
+ 
+    res, e := stmt.Exec("5")
+    ErrorCheck(e)
+ 
+    
+    a, e := res.RowsAffected()
+    ErrorCheck(e)
+ 
+    fmt.Println(a) 
+}
+ 
+func ErrorCheck(err error) {
+    if err != nil {
+        panic(err.Error())
+    }
+}
+ 
+func PingDB(db *sql.DB) {
+    err := db.Ping()
+    ErrorCheck(err)
+}
